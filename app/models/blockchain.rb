ec8dc90b8de8ff93f1e79b290ecb0801e966ec8e
Ender Ruiz <ruiz.valera@gmail.com>
2022-09-01 23:53:29 -0400

    adding css style and form validation

--- app/models/blockchain.rb
+++ app/models/blockchain.rb
@@ -1,14 +1,22 @@
 class Blockchain < ApplicationRecord
-  validates :hash_name, presence: true, uniqueness: true
-
+  validates :hash_name, presence: true
+  validates :hash_name, uniqueness: true
+  after_create :set_block_index
+  
   def add_blockchain
     if Blockchain.last.present?
       self.prev_block = Blockchain.last.hash_name
     else
       self.prev_block = ""
     end
-    self.block_index = rand(1..100)
+
     self.time = Time.now.to_s.remove('-',' ',':')[0,6]
-    self.bits = rand(100..500)
+    self.bits = rand(10000..500000)
+  end
+
+  def set_block_index
+    self.block_index = self.id
+    self.save
   end
 end
+
